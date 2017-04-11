68c7d2b3ef11371f3924be2ba068fa00332af9a8
Alexander Ellis <ellisonjtk@gmail.com>
2017-04-11 08:27:53 -0400

    Figured out zoom logic and included inline diagram

--- map.js
+++ map.js
@@ -10,12 +10,20 @@ I can mouse over the meteorite's data point for additional data.
 
 */
 
-// Select svg element
+// Create zoom element
+var zoom = d3.zoom()
+  .scaleExtent([1, 9])
+  .on('zoom', move)
+
+// Select svg element and apply zoom
 let svg = d3.select('svg')
+  .call(zoom)
 
 // Define constants
 let width = +svg.attr('width');
 let height = +svg.attr('height');
+let g;  // Initialize so we can reference in move function
+
 
 // Create projection
 let projection = d3.geoMercator()
@@ -24,7 +32,8 @@ let projection = d3.geoMercator()
 
 let path = d3.geoPath().projection(projection);
 
-//svg.call(zoom)
+
+
 
 // Get json information
 d3.json('world-topo-min.json', function(error, world) {
@@ -32,10 +41,15 @@ d3.json('world-topo-min.json', function(error, world) {
 
   let topo = countries
 
-  let g = svg.append('g')
-//    .on('click', click)
+  drawMap(topo)
 
-    g.append('path')
+})
+
+function drawMap(topo) {
+  g = svg.append('g')
+    .on('click', click)
+
+  g.append('path')
     .datum({type: 'LineString', coordinates: [[-180, 0], [-90, 0], [0, 0], [90, 0], [180, 0]]})
     .attr('class', 'equator')
     .attr('d', path);
@@ -48,7 +62,9 @@ d3.json('world-topo-min.json', function(error, world) {
     .attr('id', d => d.id)
     .attr('title', d => d.properties.name)
     .attr('fill', d => 'rgb(31,119,180)')
-})
+}
+
+
 
 // Create topojson information
 
@@ -58,7 +74,65 @@ d3.json('world-topo-min.json', function(error, world) {
 
 // draw points
 
+// move function will be hoisted for zoom assignment
+function move() {
+  let t = [d3.event.transform.x, d3.event.transform.y];
+  let s = d3.event.transform.k;
+  let zscale = s;
+  let h = height / 4;
 
-// FUNCTIONS TO DO
-//zoom
-//click
+/*
+
+Let's take a look at the maximum and minimum transformations allowed
+
+Imagine this is a map that was zoomed in.  The larger rectangle represents the full map, while the small rectangle represents the 'viewport', the html svg on the page that we see through.  The dimensions of the full rectangle will be the scale factor times the original dimensions.
+
+   _________________________
+  |         |               |
+  |         | height        |
+  |_________|               | s * height
+  |  width                  |
+  |                         |
+  |                         |
+  |_________________________|
+          s * width
+
+These t values represent the number of pixels the full rectangle is moved relative to the view port, which we can think of as static.  This means that in the above picture, we have moved it x = 0 and y = 0, as the x and y baselines are the same for both pictures.
+
+This means that no matter the size of the full rectangle, we can never move it past 0 for x and y, as that would move it to the right and down from the above picture, thus moving the full picture out of the frame.
+
+For the maximums, let's look at another picture.
+
+          s * width
+   _________________________
+  |               |         |
+  |       height  |         |
+  |               |_________| s * height
+  |                  width  |
+  |                         |
+  |                         |
+  |_________________________|
+
+  |---------------|
+  s * width - width
+
+Here, we've moved the 'viewport' all the way to the right.  We don't want it to move any farther.  This means that we have really moved the large rectangle to the left a distance of the difference between the two rectangle sizes, s * width - width.  We can factor out a width to get width * (s - 1), which gives us the number of pixels we moved the large rectangle to the left.
+
+This means that width * (s - 1) is the max movement to the left.  d3 counts movement to the left as negative, so this really means that the most we can move it is -(width * (s - 1)), or width * (1 - s).
+
+A similar argument for y yields the following expressions.
+
+*/
+  t[0] = Math.min(0, Math.max(t[0], width * ( 1 - s)))
+  t[1] = Math.min(0, Math.max(t[1], height * ( 1 - s)))
+
+  console.log(t[0])
+  g.attr('transform', 'translate(' + t + ')scale(' + s + ')');
+
+  d3.selectAll('.country').style('stroke-width', 1.5 / s);
+}
+
+function click() {
+  var latlon = projection.invert(d3.mouse(this));
+  console.log(d3.mouse(this))
+}
